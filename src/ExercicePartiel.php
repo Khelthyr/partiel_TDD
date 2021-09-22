ddead71f584e5f1a356a51ea481d96486070ae61
Khelthyr <cedric.nils.vincendeau@gmail.com>
2021-09-22 15:54:28 +0200

    End exo 1 - Completing Array + loop while with if + test numbers

--- src/ExercicePartiel.php
+++ src/ExercicePartiel.php
@@ -5,13 +5,31 @@
  */
 class RomanNumber
 {
-	public static function decimalToRoman($num)
+	public static function decimalToRoman(int $num)
 	{
 		$romanValue = '';
-		$romanListNumber = array('I' => 1, 'II' => 2 );
-		if ($num > 0 && <= 3000) {
+		$romanListNumber = array(
+		'M' => 1000,
+		'CM' => 900,
+		'D' => 500,
+		'CD' => 400,
+		'C' => 100,
+		'XC' => 90,
+		'L' => 50,
+		'XL' => 40,
+		'X' => 10,
+		'IX' => 9,
+		'V' => 5,
+		'IV' => 4,
+		'I' => 1);
+
+		while ($num > 0) {
 			foreach ($romanListNumber as $key => $value) {
-				// code...
+				if ($num >= $value) {
+					$num = $num - $value;
+					$romanValue = $romanValue . $key;
+					break;
+				}	
 			}
 		}
 		return $romanValue;
--- tests/ExercicePartielTest.php
+++ tests/ExercicePartielTest.php
@@ -13,4 +13,40 @@ final class ExercicePartielTest extends TestCase
     {
         $this->assertEquals("II", RomanNumber::decimalToRoman(2));
     }
+
+    public function testNumberIs4(): void
+    {
+        $this->assertEquals("IV", RomanNumber::decimalToRoman(4)); 
+    }
+
+    public function testNumberIs5(): void
+    {
+        $this->assertEquals("V", RomanNumber::decimalToRoman(5)); 
+    }
+
+    public function testNumberIs10(): void
+    {
+        $this->assertEquals("X", RomanNumber::decimalToRoman(10)); 
+    }
+
+    public function testNumberIs100(): void
+    {
+        $this->assertEquals("C", RomanNumber::decimalToRoman(100)); 
+    }
+
+    public function testNumberIs207(): void
+    {
+        $this->assertEquals("CCVII", RomanNumber::decimalToRoman(207)); 
+    }
+
+    public function testNumberIs1000(): void
+    {
+        $this->assertEquals("M", RomanNumber::decimalToRoman(1000)); 
+    }
+
+    public function testNumberIs3000(): void
+    {
+        $this->assertEquals("MMM", RomanNumber::decimalToRoman(3000)); 
+    }
+
 }
